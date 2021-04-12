7dcda58adc54614b6d9a819113b822d99c187f31
Maxim <schastlivy@icloud.com>
2021-04-12 14:51:08 +0300

    Homework7

---
+++ Lesson7/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        UserIntefaceView userIntefaceView = new UserIntefaceView();
+        userIntefaceView.runInterface();
+    }
+}
